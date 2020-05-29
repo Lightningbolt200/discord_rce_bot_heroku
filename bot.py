843304a9558b19fc722712b3a504b269a821d3bd
Mukunth Venkatesh <43512335+Lightningbolt200@users.noreply.github.com>
2020-05-29 20:25:02 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -39,7 +39,7 @@ async def ourteam(ctx):
     data = json.load(j)
     c=data['rating']
     d=a[0]
-    await ctx.send("Team:"+'\t\t\t'+data["name"],'\n',"Country:",'\t\t',data["country"],'\n',"Academic:",'\t\t',data["academic"],'\n',"ID:",'\t\t\t',data["id"])
+    await ctx.send(str("Team:"+'\t\t\t'+data["name"],'\n',"Country:",'\t\t',data["country"],'\n',"Academic:",'\t\t',data["academic"],'\n',"ID:",'\t\t\t',data["id"]))
     
 
 client.run(os.environ['DBToken'])
