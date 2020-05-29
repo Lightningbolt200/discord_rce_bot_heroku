99b9e9aa76848216222d38e105e7dff64651ea8f
Mukunth Venkatesh <43512335+Lightningbolt200@users.noreply.github.com>
2020-05-29 20:09:02 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -37,10 +37,9 @@ async def ourteam(ctx):
     b=subprocess.Popen(a, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE, shell=True).communicate()
     j = open("/app/down.json","r")
     data = json.load(j)
-    ctx.send(data)
     c=data['rating']
     d=a[0]
-    ctx.send(str("Team:"+'\t\t\t'+data["name"],'\n',"Country:",'\t\t',data["country"],'\n',"Academic:",'\t\t',data["academic"],'\n',"ID:",'\t\t\t',data["id"],'\n',"Aliases:",'\t\t',data["aliases"],'\n',"Current Year data:",'\t',d['2020']))
+    await ctx.send(str("Team:"+'\t\t\t'+data["name"],'\n',"Country:",'\t\t',data["country"],'\n',"Academic:",'\t\t',data["academic"],'\n',"ID:",'\t\t\t',data["id"],'\n',"Aliases:",'\t\t',data["aliases"],'\n',"Current Year data:",'\t',d['2020']))
     
 
 client.run(os.environ['DBToken'])
